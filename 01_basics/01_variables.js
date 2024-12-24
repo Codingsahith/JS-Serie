4f0ab4113f605e1a92c977cb717f0eeda978ad32
Codingsahith <161868514+Codingsahith@users.noreply.github.com>
2024-12-24 08:11:26 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const fullName = "m.sahith reddy" //we an not change the value of the variable
+var age = "21" // prefer to not to use var, because issues in block scope and functional scope
+let course = "B.tech" // we can change the value of the variables
+let place //if we don'tassign any value. it becomes as undefined valu
+branch = "cse"
+
+console.table([fullName,age,course,branch,place])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+// data types are 2 types
+// primitive data types and non-primitive datatypes
+
+// primitive datatypes
+let num = 23//integer or number
+let Name = "sahith" // string ""
+let float = 42.580 // float 0.000
+let bool = true //or false
+let bigInt = 3787908298 // big int- 78366982709
+let state = null // it is an object
+let place // undefined
+let symbols // $ @ unique values
+
+console.table([num, Name, float, bool, bigInt, state, typeof state, place, symbols])
