a189c5aed7f99be9f0791eb891d35fedfe9c35ae
Codingsahith <161868514+Codingsahith@users.noreply.github.com>
2024-12-31 07:32:55 +0000

    switch statements

---
+++ 02_basics/02_switch.js
@@ -0,0 +1,18 @@
+// Switch Statement
+
+let myAnime = ""
+
+switch(myAnime){
+    case "Blue lock" :
+        console.log("My fav anime is Blue Lock")
+        break;
+    case "Naruto" :
+        console.log("My fav anime is Naruto")
+        break;
+    case "Demon Slayers" :
+        console.log("My fav anime is",myAnime)
+        break;
+
+default :
+console.log("I dont like anime")
+}
