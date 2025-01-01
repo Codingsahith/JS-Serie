c7f606551635f188114cd98aaff0b84879524685
Codingsahith <161868514+Codingsahith@users.noreply.github.com>
2025-01-01 14:24:01 +0000

    string methods

---
+++ 02_basics/03_stringMethods.js
@@ -0,0 +1,30 @@
+// trim method 
+let myname = "  sahith " // it removes the spaces 
+// console.log (myname.trim())
+
+// upperCase method
+
+let firstname = "sahith"
+// console.log(firstname.toUpperCase())   // it converts the letter into capital letters
+
+
+// lowerCase method
+
+let lastName = "REDDY"
+// console.log(lastName.toLowerCase()) // it convets the letters into small letters
+
+// method argument
+
+let str = "sahith"
+// console.log(str.indexOf("h")) // returns the first index of occurrence of some values in string. Or give -1 if it is not found.
+// console.log(str.indexOf("s"))
+// console.log(str.indexOf("r"))
+
+// method chaining
+let str1 = "  Monkey  "
+let newStr = str1.trim()
+
+console.log(newStr.toUpperCase())
+console.log(newStr.toLowerCase())
+
+// slice method
