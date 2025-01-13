4a77c262377ee6ceed3fef6a5f242486af2448b5
Codingsahith <161868514+Codingsahith@users.noreply.github.com>
2025-01-13 07:59:00 +0000

    loops

---
+++ 03_basics/01_loops.js
@@ -0,0 +1,36 @@
+// for loop
+console.log("for loop")
+// console.log("increment")
+for(let i=0;i<=5;i++){ 
+    // console.log(i)
+}
+// console.log("decrement")
+for(let i=5;i>=0;i--){
+    // console.log(i)
+}
+
+
+// print odd numbers
+console.log("print odd numbers")
+for(let i=1;i<=9;i=i+2){
+    console.log(i)
+}
+
+// print even numbers
+console.log("print even numbers")
+for(let i=0;i<=10;i=i+2){
+    console.log(i)
+}
+
+
+// print odd numbers
+console.log("print odd numbers backward process")
+for(let i=9;i>=1;i=i-2){
+    console.log(i)
+}
+
+// print even numbers
+console.log("print even numbers backward process")
+for(let i=10;i>=0;i=i-2){
+    console.log(i)
+}
