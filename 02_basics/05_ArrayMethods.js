8a0fe8a5409ae5ebc3176ff2522e3be3297f4ef2
Codingsahith <161868514+Codingsahith@users.noreply.github.com>
2025-01-13 06:21:35 +0000

    Array Methods

---
+++ 02_basics/05_ArrayMethods.js
@@ -0,0 +1,21 @@
+// Array methods 1.push 2.pop 3.unshift 4.shift
+
+// push
+let num = [1,2,3,4]
+num.push(5)
+console.log(num) // it will push any num or str in the array
+
+// pop
+let num1 = [1,2,3,4]
+num1.pop() 
+console.log(num1) // it will pop the only last value of array
+
+// unshift 
+let arr = [1,2,3,4]
+arr.unshift(0) 
+console.log(arr) // it will add any num or str at starting in array
+
+// shift
+let arry = [1,2,3,4]
+arry.shift()
+console.log(arry) // it will remove starting value in the array
